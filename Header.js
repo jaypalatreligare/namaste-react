b4e884a6174e29ecbe1c328a99677ecbcdc14edc
Jaypal <jaypal1@religare.com>
2025-03-03 00:25:32 +0530

    toggle-login-logout-button-text

--- Header.js
+++ Header.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 export const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
         {" "}
-        <img alt="productimage"className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNl1WcEr1Wv0VlscNKDZKcM-x0WkVFKNn2Zg&s" />{" "}
+        <img
+          alt="productimage"
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNl1WcEr1Wv0VlscNKDZKcM-x0WkVFKNn2Zg&s"
+        />{" "}
       </div>
       <div className="nav-items">
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
-        </div>
+      </div>
     </div>
   );
 };
